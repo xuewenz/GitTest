9798052771722567377c6a7adc8efa593d950ec8
Xue Wen Tan <xuewen@u.nus.edu>
2019-10-08 17:38:21 +0800

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Xue Wen Tan on 8/10/19.
+//  Copyright © 2019 Xue Wen Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
